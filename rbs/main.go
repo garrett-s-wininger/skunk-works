341b0fb3152f50f48ad93ed04af3356a359505c5
Garrett Wininger <garrett.s.wininger@outlook.com>
2025-03-29 13:38:08 -0400

    Windows output exclusion, static file serving

--- rbs/main.go
+++ rbs/main.go
@@ -7,24 +7,27 @@ import (
 	"path/filepath"
 )
 
-func main() {
+func index(w http.ResponseWriter, req *http.Request) {
 	paths := []string{
 		filepath.Join("templates/index.tmpl"),
 	}
 
 	templ := template.Must(template.ParseFiles(paths...))
+	err := templ.Execute(w, nil)
 
-	// TODO(garrett): Proper multiplexing
-	handler := func(w http.ResponseWriter, req *http.Request) {
-		err := templ.Execute(w, nil)
-
-		if err != nil {
-			log.Fatal("Template execution failure: %s", err)
-		}
+	if err != nil {
+		log.Fatal("Template execution failure: %s", err)
 	}
+}
 
-	http.HandleFunc("/", handler)
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/{$}", index)
+	mux.Handle(
+		"/static/",
+		http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 
 	// TODO(garrett): Support command line flags for port selection
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
