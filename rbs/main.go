49e203f998a97d970f7b7d6fdd4ff983574abf1b
Garrett Wininger <garrett.s.wininger@outlook.com>
2025-03-29 12:44:00 -0400

    Go formatting

--- rbs/main.go
+++ rbs/main.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-        "html/template"
-        "log"
-        "net/http"
-        "path/filepath"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
 )
 
 func main() {
-        paths := []string{
-                filepath.Join("templates/index.tmpl"),
-        }
+	paths := []string{
+		filepath.Join("templates/index.tmpl"),
+	}
 
-        templ := template.Must(template.ParseFiles(paths...))
+	templ := template.Must(template.ParseFiles(paths...))
 
-        // TODO(garrett): Proper multiplexing
-        handler := func(w http.ResponseWriter, req *http.Request) {
-                err := templ.Execute(w, nil)
+	// TODO(garrett): Proper multiplexing
+	handler := func(w http.ResponseWriter, req *http.Request) {
+		err := templ.Execute(w, nil)
 
-                if err != nil {
-                        log.Fatal("Template execution failure: %s", err)
-                }
-        }
+		if err != nil {
+			log.Fatal("Template execution failure: %s", err)
+		}
+	}
 
-        http.HandleFunc("/", handler)
+	http.HandleFunc("/", handler)
 
-        // TODO(garrett): Support command line flags for port selection
-        log.Fatal(http.ListenAndServe(":8080", nil))
+	// TODO(garrett): Support command line flags for port selection
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
