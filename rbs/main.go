297d6ab815621d7f8d61e336a6d84e8aac0e73c8
Garrett Wininger <garrett.s.wininger@outlook.com>
2025-03-30 11:59:39 -0400

    Config map -> struct, unnecessary redirect removal, jwks retrieval

--- rbs/main.go
+++ rbs/main.go
@@ -22,7 +22,14 @@ var requiredConfigs = []string{
 	"oidcDomain",
 }
 
-var serverConfig map[string]string
+type ServerConfig struct {
+	ApplicationUri *url.URL
+	OidcClientId string
+	OidcClientSecret string // TODO(garrett): Investigate SGX or similar storage
+	OidcDomain *url.URL
+}
+
+var serverConfig ServerConfig
 
 func index(w http.ResponseWriter, r *http.Request) {
 	paths := []string{
@@ -38,60 +45,33 @@ func index(w http.ResponseWriter, r *http.Request) {
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
-	oidcAuthEndpoint, err := url.Parse(serverConfig["oidcDomain"])
-
-	if err != nil {
-		http.Error(w, "Invalid OIDC provider domain", http.StatusInternalServerError)
-		return
-	}
-
-	callbackUri, err := url.Parse(serverConfig["applicationUri"])
-
-	if err != nil {
-		http.Error(w, "Invalid application URI", http.StatusInternalServerError)
-		return
-	}
-
 	queryParameters := url.Values{}
 	queryParameters.Add("response_type", "code")
 	queryParameters.Add("scope", "openid profile")
-	queryParameters.Add("client_id", serverConfig["oidcClientId"])
+	queryParameters.Add("client_id", serverConfig.OidcClientId)
 
 	// TODO(garrett): This value should be bound to a session
 	queryParameters.Add("state", rand.Text())
 
-	callbackUri = callbackUri.JoinPath("oidc", "callback")
+	callbackUri := serverConfig.ApplicationUri.JoinPath("oidc", "callback")
 	queryParameters.Add("redirect_uri", callbackUri.String())
 
-	oidcAuthEndpoint = oidcAuthEndpoint.JoinPath("oauth2", "v1", "authorize")
+	oidcAuthEndpoint := serverConfig.OidcDomain.JoinPath("oauth2", "v1", "authorize")
 	oidcAuthEndpoint.RawQuery = queryParameters.Encode()
 	http.Redirect(w, r, oidcAuthEndpoint.String(), http.StatusFound)
 }
 
 func loginCallback(w http.ResponseWriter, r *http.Request) {
-	// TODO(garrett): There's no need to parse this twice, config map -> struct
-	oidcTokenEndpoint, err := url.Parse(serverConfig["oidcDomain"])
-
-	if err != nil {
-		http.Error(w, "Invalid OIDC provider domain", http.StatusInternalServerError)
-		return
-	}
-
-	callbackUri, err := url.Parse(serverConfig["applicationUri"])
-
-	if err != nil {
-		http.Error(w, "Invalid application URI", http.StatusInternalServerError)
-		return
-	}
-
+	// TODO(garrett): Split into token separate token/JWKS retrieval methods, this is getting large
+	// TODO(garrett): Validate returned state against session management to ensure we sent it
 	queryParameters := url.Values{}
 	queryParameters.Add("grant_type", "authorization_code")
 	queryParameters.Add("code", r.FormValue("code"))
 
-	callbackUri = callbackUri.JoinPath("oidc", "callback")
+	callbackUri := serverConfig.ApplicationUri.JoinPath("oidc", "callback")
 	queryParameters.Add("redirect_uri", callbackUri.String())
 
-	oidcTokenEndpoint = oidcTokenEndpoint.JoinPath("oauth2", "v1", "token")
+	oidcTokenEndpoint := serverConfig.OidcDomain.JoinPath("oauth2", "v1", "token")
 	oidcTokenEndpoint.RawQuery = queryParameters.Encode()
 
 	tokenRequest, err := http.NewRequest(
@@ -104,18 +84,17 @@ func loginCallback(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	client := &http.Client{}
-
 	tokenRequest.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-
 	tokenRequest.SetBasicAuth(
-		url.QueryEscape(serverConfig["oidcClientId"]),
-		url.QueryEscape(serverConfig["oidcClientSecret"]))
+		url.QueryEscape(serverConfig.OidcClientId),
+		url.QueryEscape(serverConfig.OidcClientSecret))
 
+	client := &http.Client{}
 	resp, err := client.Do(tokenRequest)
 
 	if err != nil {
-		log.Fatal(err)
+		http.Error(w, "Unable to perform OIDC token request", http.StatusInternalServerError)
+		return
 	}
 
 	defer resp.Body.Close()
@@ -128,19 +107,49 @@ func loginCallback(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// TODO(garrett): Handle error response from IdP, ala invalid Client Secret
-	// TODO(garrett): This is outputting a sensitive log, remove
+	// TODO(garrett): Remove, outputs sensitive data
 	log.Println(string(body))
 
-	// TODO(garrett): Use token to obtain userinfo
+	jwksRequest, err := http.NewRequest(
+		"GET",
+		serverConfig.OidcDomain.JoinPath("oauth2", "v1", "keys").String(),
+		bytes.NewBuffer([]byte{}))
+
+	if err != nil {
+		http.Error(w, "Unable to create JWKS request", http.StatusInternalServerError)
+		return
+	}
+
+	jwksRequest.Header.Add("Accept", "application/json")
+	keyResponse, err := client.Do(jwksRequest)
+
+	if err != nil {
+		http.Error(w, "Unable to perform JWKS retrieval", http.StatusInternalServerError)
+		return
+	}
+
+	defer keyResponse.Body.Close()
+
+	body, err = ioutil.ReadAll(keyResponse.Body)
+
+	if err != nil {
+		http.Error(w, "Failed to read JWKS response body", http.StatusInternalServerError)
+		return
+	}
+
+	// TODO(garrett) Validate token data, decrypt from JWKS result
+	// TODO(garrett): Remove, outputs sensitive data
+	log.Println(string(body))
+
 	// TODO(garrett): Implement session tracking
 }
 
-func parseConfiguration() (map[string]string, error) {
-	// TODO(garrett): This is ridiculously expensive, just handroll a strict parser
+func parseConfiguration() (ServerConfig, error) {
+	// TODO(garrett): This is function is not perf-aware, should be
 	contents, err := os.ReadFile(".env")
 
 	if err != nil {
-		return nil, err
+		return ServerConfig{}, err
 	}
 
 	stringData := string(contents)
@@ -155,7 +164,7 @@ func parseConfiguration() (map[string]string, error) {
 
 		if len(keyValuePair) != 2 {
 			fmt.Printf("%s: %d\n", line, len(keyValuePair))
-			return nil, fmt.Errorf("configuration: malformed line detected %s", line)
+			return ServerConfig{}, fmt.Errorf("configuration: malformed line detected %s", line)
 		}
 
 		configMap[keyValuePair[0]] = keyValuePair[1]
@@ -165,11 +174,31 @@ func parseConfiguration() (map[string]string, error) {
 		_, ok := configMap[key]
 
 		if !ok {
-			return nil, fmt.Errorf("configuration: required config %s is missing from .env", key)
+			return ServerConfig{}, fmt.Errorf("configuration: required config %s is missing from .env", key)
 		}
 	}
 
-	return configMap, nil
+
+	applicationUri, err := url.Parse(configMap["applicationUri"])
+
+	if err != nil {
+		return ServerConfig{}, fmt.Errorf("configuration: applicationUri is not a valid URI")
+	}
+
+	oidcDomain, err := url.Parse(configMap["oidcDomain"])
+
+	if err != nil {
+		return ServerConfig{}, fmt.Errorf("configuration: oidcDomain is not a valid URI")
+	}
+
+	parsedConfig := ServerConfig{
+		ApplicationUri: applicationUri,
+		OidcClientId: configMap["oidcClientId"],
+		OidcClientSecret: configMap["oidcClientSecret"],
+		OidcDomain: oidcDomain,
+	}
+
+	return parsedConfig, nil
 }
 
 func main() {
@@ -186,7 +215,9 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/{$}", index)
+	mux.HandleFunc("/login", login)
 	mux.HandleFunc("/login/{$}", login)
+	mux.HandleFunc("/oidc/callback", loginCallback)
 	mux.HandleFunc("/oidc/callback/{$}", loginCallback)
 	mux.Handle(
 		"/static/",
