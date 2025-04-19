eedec18c33cc62fe89cbb2c478a6e38015e1f58d
Garrett Wininger <garrett.s.wininger@outlook.com>
2025-04-19 18:59:40 -0400

    HTTP/2 & TLS Support

--- rbs/main.go
+++ rbs/main.go
@@ -11,6 +11,8 @@ var serverConfig ServerConfig
 
 func main() {
 	port := flag.Int("p", 8080, "Requested port number to listen on")
+	cert := flag.String("c", "", "Location of certificate for HTTP2/TLS serving")
+	key := flag.String("k", "", "Location of private key for HTTP2/TLS service")
 	flag.Parse()
 
 	config, err := parseConfiguration()
@@ -57,5 +59,17 @@ func main() {
 		http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 
 	listenAddress := fmt.Sprintf("0.0.0.0:%d", *port)
-	log.Fatal(http.ListenAndServe(listenAddress, mux))
+
+	if (*cert == "" && *key != "") || (*cert != "" && *key == "") {
+		log.Fatal("Detected HTTP2/TLS settings, both certificate (c) and key (k) must be provided")
+	}
+
+	// TODO(garrett): Check on the possibility of automatically reloading the TLS context to prevent
+	// necessity for application restart (FSEvents/inotify/FileWatch)
+
+	if (*cert == "" && *key == "") {
+		log.Fatal(http.ListenAndServe(listenAddress, mux))
+	} else {
+		log.Fatal(http.ListenAndServeTLS(listenAddress, *cert, *key, mux))
+	}
 }
