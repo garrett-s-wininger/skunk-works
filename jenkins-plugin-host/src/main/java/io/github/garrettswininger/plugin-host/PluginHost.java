38aeb3867903f25e73800104a3b4992bd0692f63
Garrett Wininger <garrett.s.wininger@outlook.com>
2025-10-10 22:00:53 -0400

    Plugin host basic skeleton

---
+++ jenkins-plugin-host/src/main/java/io/github/garrettswininger/plugin-host/PluginHost.java
@@ -0,0 +1,106 @@
+package io.github.garrettswininger.pluginhost;
+
+import hudson.Plugin;
+import java.io.File;
+import java.io.IOException;
+import java.lang.Thread;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.util.concurrent.TimeUnit;
+import java.util.List;
+import java.util.logging.Logger;
+
+import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
+
+public class PluginHost extends Plugin {
+    private final static Logger LOGGER = Logger.getLogger(
+        PluginHost.class.getName()
+    );
+
+    void createDirectoryIfNotExists(File dir) {
+        if (!dir.exists()) {
+            try {
+                Files.createDirectory(dir.toPath());
+            } catch (IOException ex) {
+                LOGGER.severe(
+                    String.format("Failed to create directory: %s", ex.getMessage())
+                );
+            }
+        }
+    }
+
+    @Override
+    public void start() {
+        final var dataDir = Utility.getDataDirectory();
+        final var autoloadDir = Utility.getAutoloadDirectory();
+        final var requiredDirs = List.of(dataDir, autoloadDir);
+
+        requiredDirs.forEach(dir -> createDirectoryIfNotExists(dir));
+        LOGGER.info("All required directories are present or have been created.");
+
+        final var fs = FileSystems.getDefault();
+
+        final var dirWatcherDaemon = new Thread(() -> {
+            try (final var watcher = fs.newWatchService()) {
+                autoloadDir.toPath().register(
+                    watcher,
+                    ENTRY_CREATE,
+                    ENTRY_DELETE,
+                    ENTRY_MODIFY
+                );
+
+                LOGGER.info("Hosted plugin autoloading now operational.");
+
+                while (true) {
+                    try {
+                        final var key = watcher.poll(10, TimeUnit.SECONDS);
+
+                        if (key == null) {
+                            continue;
+                        }
+
+                        // NOTE(garrett): We'll want to actually re-order the
+                        // events here to handle deletions first. In the event
+                        // of a rename, we get a creation and deletion but these
+                        // may not be ordered from the underlying watch system
+                        for (final var event: key.pollEvents()) {
+                            final var kind = event.kind();
+
+                            if (kind == ENTRY_CREATE) {
+                                LOGGER.info("File created");
+                            } else if (kind == ENTRY_DELETE) {
+                                LOGGER.info("File deleted");
+                            } else if (kind == ENTRY_MODIFY) {
+                                LOGGER.info("File modified");
+                            } else {
+                                LOGGER.info("Overflow - Some events not delivered");
+                            }
+                        }
+
+                        key.reset();
+                    } catch (InterruptedException ex) {
+                        LOGGER.severe(
+                            "Watching thread interrupt, hosted plugins now frozen."
+                        );
+                    }
+                }
+            } catch (IOException ex) {
+                LOGGER.severe(
+                    String.format(
+                        "Failed to create autoload directory watcher: %s",
+                        ex.getMessage()
+                    )
+                );
+            }
+        });
+
+        dirWatcherDaemon.setName("Plugin Host Filesystem Watcher");
+        dirWatcherDaemon.setDaemon(true);
+        dirWatcherDaemon.start();
+    }
+}
