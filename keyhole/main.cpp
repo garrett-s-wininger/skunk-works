2a0f516c90c815438e44fbc55f8eb74ba649e37f
Garrett Wininger <garrett.s.wininger@outlook.com>
2025-12-06 18:53:58 -0500

    [WIP] Endianness handling

--- keyhole/main.cpp
+++ keyhole/main.cpp
@@ -2,9 +2,11 @@
 #include <ctime>
 #include <filesystem>
 #include <format>
+#include <fstream>
 #include <print>
 #include <string>
 #include <string_view>
+#include <vector>
 
 using namespace std::literals;
 
@@ -40,6 +42,67 @@ void log(std::string_view prefix, std::string_view message) {
 void error(std::string_view message) {
     log(level_prefix<Error>(), message);
 }
+
+}
+
+// TODO(garrett): Separate out when larger/more concrete
+namespace bytecode {
+
+constexpr uint32_t CLASS_FILE_MAGIC_VALUE = 0xCAFEBABE;
+
+template <typename T>
+concept JVMLSMultiByte =
+    std::is_same_v<T, uint16_t> || std::is_same_v<T, uint32_t>;
+
+template <JVMLSMultiByte T>
+T read_multi_byte_value(std::ifstream& content_stream) {
+    T data{};
+
+    content_stream.read(reinterpret_cast<char*>(&data), sizeof(T));
+
+    if constexpr (std::endian::native == std::endian::big) {
+        return data;
+    } else {
+        return std::byteswap(data);
+    }
+
+    return data;
+}
+
+class ClassFile {
+private:
+    std::ifstream content_stream;
+public:
+    ClassFile(std::filesystem::path path) {
+        const auto path_string = path.string();
+
+        std::ifstream content_stream(
+            path_string,
+            std::ios::binary
+        );
+
+        if (!content_stream.is_open()) {
+            throw std::runtime_error(
+                std::format("Unable to open class file ({})", path_string)
+            );
+        }
+
+        auto loaded_magic = read_multi_byte_value<uint32_t>(content_stream);
+
+        if (loaded_magic != CLASS_FILE_MAGIC_VALUE) {
+            throw std::runtime_error(
+                std::format(
+                    "Class file ({}) contains invalid magic value: 0x{:08X} != 0x{:08X}",
+                    path_string,
+                    loaded_magic,
+                    CLASS_FILE_MAGIC_VALUE
+                )
+            );
+        }
+
+        // TODO(garrett): More class file-y things
+    }
+};
 
 }
 
@@ -82,6 +145,12 @@ int main(int argc, char** argv) {
     }
 
     // TODO(garrett): File processing
+    try {
+        bytecode::ClassFile checked_file(target);
+    } catch (const std::runtime_error& e) {
+        logging::error(e.what());
+        return EXIT_FAILURE;
+    }
 
     return EXIT_SUCCESS;
 }
