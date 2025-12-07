e89728e3436a2ac901958f1b80a51db4595b0d86
Garrett Wininger <garrett.s.wininger@outlook.com>
2025-12-07 17:02:42 -0500

    [WIP] Some initial ConstantPool parsing

--- keyhole/main.cpp
+++ keyhole/main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <array>
 #include <ctime>
 #include <filesystem>
@@ -6,6 +7,7 @@
 #include <print>
 #include <string>
 #include <string_view>
+#include <variant>
 #include <vector>
 
 using namespace std::literals;
@@ -55,7 +57,7 @@ concept JVMLSMultiByte =
     std::is_same_v<T, uint16_t> || std::is_same_v<T, uint32_t>;
 
 template <JVMLSMultiByte T>
-T read_multi_byte_value(std::ifstream& content_stream) {
+auto read_multi_byte_value(std::ifstream& content_stream) -> T{
     T data{};
 
     content_stream.read(reinterpret_cast<char*>(&data), sizeof(T));
@@ -69,11 +71,104 @@ T read_multi_byte_value(std::ifstream& content_stream) {
     return data;
 }
 
+constexpr auto jdk_version(const uint16_t bytecode_major_version) -> uint8_t {
+    if (bytecode_major_version < 49) {
+        // NOTE(garrett): Bundle 1.0-1.4 just because we're being lazy here.
+        return 1;
+    }
+
+    // NOTE(garrett): Starting at JDK 5, there's a 44 number wide gap between
+    // the major version and the named release number.
+    return bytecode_major_version - 44;
+}
+
+struct ClassFileVersion {
+    uint16_t major_version;
+    uint16_t minor_version;
+};
+
+enum class ConstantPoolTag : uint8_t {
+    UTF8 = 1,
+    Class = 7,
+    MethodReference = 10,
+    NameAndType = 12
+};
+
+constexpr std::array<ConstantPoolTag, 4> CLASS_FILE_CONSTANT_POOL_TAGS {
+    ConstantPoolTag::UTF8,
+    ConstantPoolTag::Class,
+    ConstantPoolTag::MethodReference,
+    ConstantPoolTag::NameAndType
+};
+
+struct ConstantPoolClass {
+    uint16_t name_index;
+};
+
+struct ConstantPoolMethodReference {
+    uint16_t class_index;
+    uint16_t name_and_type_index;
+};
+
+struct ConstantPoolNameAndType {
+    uint16_t name_index;
+    uint16_t descriptor_index;
+};
+
+struct ConstantPoolUTF8 {
+    // NOTE(garrett): Going for simplicity, this is NOT proper unicode handling
+    std::string text;
+};
+
+using ConstantPoolEntry = std::variant<
+    ConstantPoolClass,
+    ConstantPoolMethodReference,
+    ConstantPoolNameAndType,
+    ConstantPoolUTF8
+>;
+
+auto make_constant_pool_entry(ConstantPoolTag tag, std::ifstream& content_stream) -> ConstantPoolEntry {
+    // FIXME(garrett): One of these is parsing extra or too-little data
+    switch (tag) {
+        case ConstantPoolTag::Class: {
+            auto data = ConstantPoolClass{};
+            data.name_index = read_multi_byte_value<uint16_t>(content_stream);
+            return data;
+        }
+        case ConstantPoolTag::MethodReference: {
+            auto data = ConstantPoolMethodReference{};
+            data.class_index = read_multi_byte_value<uint16_t>(content_stream);
+            data.name_and_type_index = read_multi_byte_value<uint16_t>(content_stream);
+            return data;
+        }
+        case ConstantPoolTag::NameAndType: {
+            auto data = ConstantPoolNameAndType{};
+            data.name_index = read_multi_byte_value<uint16_t>(content_stream);
+            data.descriptor_index = read_multi_byte_value<uint16_t>(content_stream);
+            return data;
+        }
+        case ConstantPoolTag::UTF8: {
+            auto data = ConstantPoolUTF8{};
+            auto string_length = read_multi_byte_value<uint16_t>(content_stream);
+
+            data.text = std::string(string_length, '\0');
+            content_stream.read(data.text.data(), data.text.capacity());
+
+            return data;
+        }
+    }
+
+    throw std::logic_error("Exhausted tag implementations or fell through");
+}
+
 class ClassFile {
 private:
-    std::ifstream content_stream;
+    ClassFileVersion version;
+    std::vector<ConstantPoolEntry> constant_pool;
 public:
-    ClassFile(std::filesystem::path path) {
+    ClassFile() : version(ClassFileVersion{55, 0}) {}
+
+    ClassFile(const std::filesystem::path& path) {
         const auto path_string = path.string();
 
         std::ifstream content_stream(
@@ -87,7 +182,7 @@ public:
             );
         }
 
-        auto loaded_magic = read_multi_byte_value<uint32_t>(content_stream);
+        const auto loaded_magic = read_multi_byte_value<uint32_t>(content_stream);
 
         if (loaded_magic != CLASS_FILE_MAGIC_VALUE) {
             throw std::runtime_error(
@@ -100,13 +195,62 @@ public:
             );
         }
 
-        // TODO(garrett): More class file-y things
+        // NOTE(garrett): JVMLS states the class file format is minor and then
+        // major, section 4.1 - The ClassFile Structure
+        version = ClassFileVersion{};
+        version.minor_version = read_multi_byte_value<uint16_t>(content_stream);
+        version.major_version = read_multi_byte_value<uint16_t>(content_stream);
+
+        const auto constant_pool_count = read_multi_byte_value<uint16_t>(content_stream);
+        constant_pool = std::vector<ConstantPoolEntry>(constant_pool_count);
+
+        for (auto i = 0; i < constant_pool_count; ++i) {
+            uint8_t tag{};
+            content_stream.read(reinterpret_cast<char*>(&tag), sizeof(tag));
+
+            auto is_valid = std::ranges::any_of(
+                CLASS_FILE_CONSTANT_POOL_TAGS,
+                [=](const auto known_tag) {
+                    return static_cast<uint8_t>(known_tag) == tag;
+                }
+            );
+
+            if (!is_valid) {
+                throw std::runtime_error(
+                    std::format("Encountered unknown tag value: {}", tag)
+                );
+            }
+
+            auto entry = make_constant_pool_entry(
+                static_cast<ConstantPoolTag>(tag),
+                content_stream
+            );
+
+            constant_pool.push_back(entry);
+
+            // TODO(garrett): Remove, more constant pool parsing
+            std::visit([](auto&& value) {
+                using T = std::decay_t<decltype(value)>;
+
+                if constexpr (std::is_same_v<T, ConstantPoolUTF8>) {
+                    std::println("UTF-8 Value: {}", value.text);
+                }
+            }, entry);
+        }
+    }
+
+    auto major_version() const -> uint16_t {
+        return version.major_version;
+    }
+
+    auto minor_version() const -> uint16_t {
+        return version.minor_version;
     }
 };
 
 }
 
-void help() {
+auto help() -> void {
     std::println(stderr, "keyhole CLI v0.1.0\n");
     std::println(stderr, "Provides introspection and instrumentation for JVM bytecode\n");
     std::println(stderr, "USAGE");
@@ -115,13 +259,13 @@ void help() {
     std::println(stderr, "  -h, --help print this message");
 }
 
-void usage() {
+auto usage() -> void {
     std::println(stderr, "Usage:");
     std::println(stderr, "  keyhole (-h|--help)");
     std::println(stderr, "  keyhole file");
 }
 
-int main(int argc, char** argv) {
+auto main(int argc, char** argv) -> int {
     if (argc != 2) {
         usage();
         return EXIT_FAILURE;
@@ -144,9 +288,16 @@ int main(int argc, char** argv) {
         return EXIT_FAILURE;
     }
 
-    // TODO(garrett): File processing
     try {
-        bytecode::ClassFile checked_file(target);
+        bytecode::ClassFile class_file{target};
+        auto major = class_file.major_version();
+
+        std::println(
+            "ClassFile version {}.{} (Java {})",
+            major,
+            class_file.minor_version(),
+            bytecode::jdk_version(major)
+        );
     } catch (const std::runtime_error& e) {
         logging::error(e.what());
         return EXIT_FAILURE;
